093495776aabff327df2cb02796f8d70ec37c94c
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2023-06-30 22:06:56 +0700

    Update main.go

--- main.go
+++ main.go
@@ -17,18 +17,18 @@ func main() {
 		panic(err.Error())
 	}
 	// Open gorm database connection
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err.Error())
-	}
-	log.Println("Database connection established")
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	// 	os.Getenv("DB_USER"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_HOST"),
+	// 	os.Getenv("DB_PORT"),
+	// 	os.Getenv("DB_NAME"),
+	// )
+	// _, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// log.Println("Database connection established")
 	router := gin.New()
 	router.Use(
 		func(c *gin.Context) {
